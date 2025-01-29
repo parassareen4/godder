0470e01abb713fbfc7dc4f404e9dcfcd9a711042
parassareen4 <kyakrnahaibro@gmail.com>
2025-01-29 18:17:55 -0500

    ddd

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -3,19 +3,23 @@ import { HfInference } from "@huggingface/inference";
 dotenv.config();
 const client = new HfInference(process.env.HUGGINGFACE_API_KEY);
 let out = "";
-const stream = client.chatCompletionStream({
-    model: "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B",
-    messages: [
-        { role: "user", content: "hey wassup\n" }
-    ],
-    temperature: 0.5,
-    max_tokens: 2048,
-    top_p: 0.7
-});
-for await (const chunk of stream) {
-    if (chunk.choices && chunk.choices.length > 0) {
-        const newContent = chunk.choices[0].delta.content;
-        out += newContent;
-        console.log(newContent);
+async function getAnswer(input) {
+    const stream = client.chatCompletionStream({
+        model: "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B",
+        messages: [
+            { role: "user", content: input }
+        ],
+        temperature: 0.5,
+        max_tokens: 1000,
+        top_p: 0.7
+    });
+    for await (const chunk of stream) {
+        if (chunk.choices && chunk.choices.length > 0) {
+            const newContent = chunk.choices[0].delta.content;
+            out += newContent;
+            process.stdout.write(newContent.replace(/\n/g, '') + '');
+        }
     }
+    process.stdout.write('\n');
 }
+getAnswer("What is the capital ddss why say it in a funny way");
